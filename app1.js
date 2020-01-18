ea9d789956fbc9fbaf5dd00672f56687f0a216b9
ribo <roibeard.mac.unfraidh@gmail.com>
2020-01-18 15:12:48 +0100

    Center middle background.

--- app1.js
+++ app1.js
@@ -2,8 +2,12 @@
 var slide = 0;
 let lastSlide = 29;
 // import TypeWriting from 'typewriting'?;
+            let textTop = [
+                `2`,
+                
+              ];
 let textBottom = [
-
+    
     ``,
   `I am the wind which breaths upon the sea
   I am the wave of the ocean
@@ -24,28 +28,24 @@ let textBottom = [
    an Enchantment of Winds.`
   
 ];
-let textTop = [
-    ``,
-    `I am the wind which breaths upon the sea
-    I am the wave of the ocean
-    I am the murmur of the billows`,
-      
-    `I am Stag of Seven Tines
-    I am a Hawk on a Cliff
-    I am a beam of the sun
-    I am the fairest of plants`,
-        
-    `I am a salmon in the water
-    I am a wild boar in valor
-    I am a lake in the plain`,
-    
-    `I am a word of science
-     I am the point of the lance of battle
-     ...(I am) a Song on a Spear
-     an Enchantment of Winds.`
-    
-  ];
 let images = [
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
+    `placeHolder0.png`,
     `placeHolder0.png`,
     `placeHolder0.png`,
     `placeHolder0.png`
@@ -65,7 +65,7 @@ $('#prev').on('touchend', function(){
 })
 
 $('#done').on('touchend', function(){
-    
+alert('done pressed')    
     // redirect
     // render();
 })
@@ -77,25 +77,29 @@ render = () =>{
 
 
     //clear top middle and bottom sections
-    $('#top #top-content').remove();
-    $('#middle #middle-content').remove();
-    $('#bottom #bottom-content').remove();
+    // $('#middle').empty();
+    // $('#bottom').empty();
 
-    // alert(slide);
         //load new content into top middle bottom sections
+        
         
         setTimeout(function(){
             
-            
-            $('#top').append(`<h2 id="top-content">${textTop[slide]}</h2>`).fadeIn()
-            
-            
-        },1000);
+            $('#top').empty().append(`<h3 id="top-content">${textTop[slide]}</h3>`);
+            $('#top-content').fadeIn()
         
-        $('#top').fadeOut()
+            $('#middle').empty().css('background-image', `url('./images/About1/${images[slide]}')`);
+        
+            
+        },600);
+        
+        $('#top-content').fadeOut();
+        $('#middle-content').fadeOut();
         
         
-        // $('#middle').append(`<img id="middle-content" src="./images/About1/${images[slide]}">`)
+        
+        $('#middle-content').fadeIn()
+
         // $('#bottom').append(`<h2 id="bottom-content">${textBottom[slide]}</h2>`)
 
 
