b62110a22c8077a5241536fe4db8fb402957c15c
ribo <roibeard.mac.unfraidh@gmail.com>
2020-01-23 10:52:15 +0100

    style screen-blocker.

--- app1.js
+++ app1.js
@@ -1,5 +1,6 @@
 
 var slide = 0;
+var poemSlide = 7;
 let lastSlide = 29;
 // import TypeWriting from 'typewriting'?;
             let textTop = [
@@ -7,28 +8,77 @@ let lastSlide = 29;
     ``,
 
     `Fadó fadó...`,
+    
+    `Fadó fadó...`,
+    `Fadó fadó...`,
+    `Fadó fadó...`,
+    `Fadó fadó...`,
+    `Fadó fadó...`,
+    `Am gaeth i m-muir
+    Am tond trethan
+    Am fuaim mara
+    `,
+    `Am dam secht ndirend
+    Am séig i n-aill
+    Am dér gréne
+    Am cain lubai
+    `,
+    `Am torc ar gail
+    Am he i l-lind
+    Am loch i m-maig
+    Am bri danae
+    Am bri 
+        i fodb fras feochtu
+    Am dé delbas
+             do chind codnu
+    `,
+    `...Cáinte im gai 
+    cainte gaithe.
+    `,
     ``,
     ``,
     ``,
-    ``,
-    ``,
-              
+    
+
+    `1169 AD`
+
                 
               ];
 let textBottom = [
     
     ``,
     `
-    An Iberian tribe                 
-    called the Milesians
-    arrived at the island now called 
-    Ireland 
-     
+    According to legend,
+    Gaelic people descend
+    from an Iberian tribe
+    called the Milesians. 
     `,
-    ``,
-    ``,
-    ``,
-  `I am the wind which breaths upon the sea
+    `Breogán son of Brath
+    son of Bile Galamh Míl Espáine
+    had seven sons 
+    `,
+`    One of whom
+     was a poet
+    called 
+    Amergin 
+    the Milesian.
+    `,
+    `Amergin 
+    led the Milesians
+     to Ireland, 
+    then ruled by 
+    a tribe called 
+     
+    Dé Dannan.`,
+    `The druids of Dé Danann 
+    raised a magical storm 
+    to keep Amergín’s 
+    expedition from reaching land. 
+    `,
+    `But Amergín was a poet
+    and words have power`,
+
+    `I am the wind which breaths upon the sea
   I am the wave of the ocean
   I am the murmur of the billows`,
     
@@ -45,15 +95,86 @@ let textBottom = [
    I am the point of the lance of battle
    ...(I am) a Song on a Spear
    an Enchantment of Winds.`
-  
+,  
+`The storm calmed.`,
+`The kings of Dé Danann 
+were killed 
+in single combat
+ by the sons of Míl Espáine.`
+
+,
+`Amergin 
+named the land after the queens
+of Dé Dannan:
+
+Banba, Fódla and Ériu 
+hence Éire.`
+,
+`The Dé Danann 
+retreated into the Earth,
+and for thousands of years 
+Gaelic civilization 
+flourished in Ireland.`
+,
+
+`In 1169 AD 
+Norman Mercenaries were invited to Ireland 
+by the Gaelic king Diarmait Mac Murchada to help him control the province of Leinster.`
+,
+`So began 800 years of English rule in Ireland.`
+,
+`Over the centuries, Norman warlords sent to conquer became integrated into Gaelic society.`
+,
+`Subsequent waves of Crown forces faced the descendents of previous invaders who had become 
+
+"More Irish than the Irish themselves"`
+,
+`By the start of the sixteenth century,
+despite centuries of military campaigning,
+English law existed only within
+ ‘The Pale’, 
+a small area of control surrounding Dublin Castle.`
+,
+
+`In 1580 yet another Gaelic rebellion was defeated.
+The survivors were massacred on the orders of the English commander Lord Grey.`
+,
+`Grey’s  servant, the great poet Edmund Spencer wrote to Queen Elizebeth I, explaining why the Irish kept resisting English rule.`
+,
+`In his view, Ireland would never be totally 
+"pacified" by the English until 
+its indigenous language and customs had been destroyed.`
+,
+`"Soe that the speach being Irish, 
+the hart must needes be Irishe; 
+for out of the aboundance of the hart, 
+the tonge speaketh"`
+,
+`Edmund Spencer
+A Veue of the present state of Irelande
+1596`
+,
+`It is the contention of this app that
+Edmund Spencer was correct.
+Language isn't merely communication.`
+,
+`Language is involved with the architecture of our synapses; with how we form thought.`
+
+
+
+
+
+
+
 ];
 let images = [
     `placeHolder0.png`,
     `placeHolder0.png`,
-    `placeHolder0.png`,
-    `placeHolder0.png`,
-    `placeHolder0.png`,
-    `placeHolder0.png`,
+    `a.png`,
+    `blue-cape.gif`,
+    `b.png`,
+    `c.png`,
+    ``,
     `placeHolder0.png`,
     `placeHolder0.png`,
     `placeHolder0.png`,
